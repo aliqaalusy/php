c1b68e65eaaa23aae0ee5418f4ea5062009762bc
aliqaaa <syabilaaliqaluthfiana@gmail.com>
2026-05-07 14:55:40 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,7 @@
 <?php
-// PHP akan mencoba mengambil data asli dari Railway dulu, 
-// kalau tidak ada baru pakai data di dalam kutip.
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
 $pass = getenv('MYSQLPASSWORD') ?: "LZstNChgUtUIzpLIpzjVtJVusCMZscPX";
@@ -10,7 +11,9 @@ $port = getenv('MYSQLPORT') ?: 3306;
 $koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$koneksi) {
-    // Menampilkan error yang lebih spesifik jika gagal
-    die("Koneksi gagal: " . mysqli_connect_error());
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
+
+// 5. Pesan Sukses (Opsional - hapus jika sudah berhasil konek)
+// echo "Koneksi Berhasil Terhubung!";
 ?>
